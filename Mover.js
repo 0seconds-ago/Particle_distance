69f1d9bb5a87c01027b44fc1c1ee5c85d6dbe2ed
0seconds <123317581+0seconds-ago@users.noreply.github.com>
2023-11-03 01:12:03 +0900

    addConnection

--- Mover.js
+++ Mover.js
@@ -6,6 +6,7 @@ class Mover {
     this.yl = random(height);
     this.rate = r;
     this.speed = s;
+    this.connections = [];
 
 
   }
@@ -18,5 +19,28 @@ class Mover {
     //? lerp(어디서, 어디로, 얼마나 빠르게);
     this.x = lerp(this.x, this.xl, this.speed);
     this.y = lerp(this.y, this.yl, this.speed);
+
   };
+
+  addConnection(otherMover) {
+    this.connections.push(otherMover);
+  }
+
+  drawConnection(otherMover) {
+    let d = dist(this.x, this.y, otherMover.x, otherMover.y);
+    if (d < r) {
+      let sw = map(d, 0, r, 15, 1);
+      let sf = map(sw, 20, 1, 255, 50);
+      strokeWeight(sw);
+      stroke(255, sf);
+      line(this.x, this.y, otherMover.x, otherMover.y);
+    }
+}
+
+  draw() {
+    strokeWeight(20);
+    stroke(255, 180, 50, 255);
+    point(this.x, this.y);
+
+}
 }
--- sketch.js
+++ sketch.js
@@ -1,49 +1,46 @@
-let heading = [];
+let movers = [];
 let limit = 10;
-let r = 100;
+let r = 500;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(0,255);
 
   // for (let i = 0; i < limit; i++){
-  //   heading.push(new Mover(0.005,0.01));
+  //   movers.push(new Mover(0.005,0.01));
   // }
 
-  background(0,255);
 }
 
 function draw() {
   background(0,255);
 
-  for (let i = 0; i < heading.length; i++){
-    heading[i].update();
-    let xh = heading[i].x;
-    let yh = heading[i].y;
 
-    for (let ii=0; ii< heading.length; ii++){
-      let xht = heading[ii].x;
-      let yht = heading[ii].y;
-      let d = dist(xh, yh, xht, yht);
-      if (d < r && i !== ii){
-        let sw = map(d,0,r,15,1);
-        let sf = map(sw,20,1,255,50);
-        strokeWeight(sw);
-        stroke(255,sf);
-        line(xh,yh,xht,yht);        
+  for (let i = 0; i < movers.length; i++) {
+    movers[i].update();
+
+    for (let ii = 0; ii < movers.length; ii++) {
+      if(i !== ii){
+        movers[i].drawConnection(movers[ii]);
       }
-    }
+    }    
 
-    strokeWeight(20);
-    stroke(255,180,50,255);
-    point(xh,yh);
+  movers[i].draw();
     
   }
 }
 
 function mouseClicked(){
-  heading.push(new Mover(0.005,0.01));
+  let newMover = new Mover(0.005,0.01);
+  movers.push(newMover);
 
-  if (heading.length > limit){
-    heading.splice(1,1);
+  if (movers.length > limit){
+    movers.shift();
   }
+
+  for (let i = 0; i < movers.length - 1; i++) {
+    newMover.addConnection(movers[i]);
+  }
+
 }
