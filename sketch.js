ecec758f06a0eb46b40e5335418f26f0b11d2d7b
0seconds <123317581+0seconds-ago@users.noreply.github.com>
2023-11-02 22:13:02 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+let heading = [];
+let popu = 25;
+let r = 75;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  background(0,255);
+}
+
+function draw() {
+  background(255);
+}
