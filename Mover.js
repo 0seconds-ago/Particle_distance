b62bb8747d9ef90d138e4ed76f41d8307281c564
0seconds <123317581+0seconds-ago@users.noreply.github.com>
2023-11-03 16:32:51 +0900

    Hanging-ball update

--- Mover.js
+++ Mover.js
@@ -2,8 +2,8 @@ class Mover {
   constructor(r, s) {
     this.pos = createVector(mouseX, mouseY);
     this.target = createVector(random(width), random(height));
-    this.vel = createVector(0,0);
-    this.acc = createVector(0,0);
+    this.vel = createVector(0, random(-7, 5));
+    this.acc = createVector(0,random(-5, 5));
     this.rate = r;
     this.speed = s;
     this.connections = [];
@@ -15,12 +15,12 @@ class Mover {
     if (mouseIsPressed) {
       this.acc.add(aForce);
     }
-    else{this.acc.sub(aForce);
+    else{this.acc.sub(aForce).mult(0.3);
     }
 }
 
   update() {
-    let gravity = createVector(0,0.05);
+    let gravity = createVector(0,0.1).sub(this.x,this.y);
     this.applyForce(gravity);
 
     this.vel.add(this.acc);
@@ -31,8 +31,8 @@ class Mover {
       this.target = createVector(random(width), random(height));
     }
 
-    this.pos.x = lerp(this.pos.x, this.target.x, this.speed);
-    this.pos.y = lerp(this.pos.y, this.target.y, this.speed);
+    // this.pos.x = lerp(this.pos.x, this.target.x, this.speed);
+    // this.pos.y = lerp(this.pos.y, this.target.y, this.speed);
     
     this.acc.mult(0);
 
@@ -51,21 +51,38 @@ class Mover {
   
   
   drawSil(other) {
-    let d = dist(this.pos.x, this.pos.y, other.x, other.y);
+    let d = dist(this.pos.x, this.pos.y, this.pos.x, 0);
     if (d < silr) {
       let sw = map(d, 0, silr, sils, 1);
       let sf = map(sw, 20, 1, 255, 50);
-      strokeWeight(sw);
+      strokeWeight(sw+this.vel.y);
       stroke(255, sf);
-      line(this.pos.x, this.pos.y, other.x, other.y);
+      // quad(this.pos.x, this.pos.y, this.pos.x,0, 
+      //   this.pos.x, this.pos.y, this.x, 0, +10, 2, 7);
+      line(this.pos.x, this.pos.y, this.pos.x,0);
     }
   }
 
 
   draw() {
-    strokeWeight(bs);
+    bs = this.vel.y;
+    strokeWeight((bs+30)*5);
     stroke(255, 180, 50, 255);
     point(this.pos.x, this.pos.y);
+  }
+
+  checkEdges() {
+    // if (this.pos.x > width || this.pos.x < 0) {
+    //   this.vel.x *= -1;
+    // }
+
+    if (this.pos.y > height || this.pos.y < 0) {
+      if (this.pos.y > height) {
+        this.pos.y = height;
+      }
+      this.vel.y *= -0.8;
+
+    }
   }
   
 }
